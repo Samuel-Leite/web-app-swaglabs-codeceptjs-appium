002a87536906a75102c04c977b77db5759dd863d
Leite <slle@gft.com>
2023-02-12 16:23:44 -0300

    include bdd scenarios in web tests

--- codecept.conf.js
+++ codecept.conf.js
@@ -5,15 +5,15 @@ exports.config = {
       platform: 'Android',
       app: 'C:/Users/slle/OneDrive - GFT Technologies SE/Desktop/Samuel Leite/AutomationProject/Mobile/app/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk',
       desiredCapabilities: {
-        // appPackage: 'com.swaglabsmobileapp',
-        // appActivity: 'MainActivity',
-        // deviceName: 'pixel',
-        // platformVersion: '9'
-
+        appPackage: 'com.swaglabsmobileapp',
+        appActivity: 'MainActivity',
         deviceName: 'pixel',
-        platformVersion: '9',
-        automationName: 'UiAutomator2',
-        browserName: 'Chrome'
+        platformVersion: '9'
+
+        // deviceName: 'pixel',
+        // platformVersion: '9',
+        // automationName: 'UiAutomator2',
+        // browserName: 'Chrome'
 
       }
     }
@@ -34,10 +34,10 @@ exports.config = {
   timeout: null,
   teardown: null,
   hooks: [],
-  // gherkin: {
-  //   features: './features/*.feature',
-  //   steps: ['./step_definitions/app_steps.js']
-  // },
+  gherkin: {
+    features: './features/*.feature',
+    steps: ['./step_definitions/app_steps.js']
+  },
   plugins: {
     screenshotOnFail: {
       enabled: true
@@ -70,5 +70,5 @@ exports.config = {
     }
   ],
   name: 'mobile-automation',
-  tests: './steps/web_test.js'
+  // tests: './steps/app_test.js'
 }
